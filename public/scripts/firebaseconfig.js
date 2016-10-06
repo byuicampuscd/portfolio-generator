01a0e4992edafa8865b1ab540bafba56bb115bd9
andrewgremlich <andrew.gremlich@gmail.com>
2016-10-06 08:47:14 -0600

    Sacking...

--- public/scripts/firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyCoJYDEPTVm4C5DvZlITgnBizmxi3AS6wc",
-    authDomain: "portfolio-generator.firebaseapp.com",
-    databaseURL: "https://portfolio-generator.firebaseio.com",
-    storageBucket: "portfolio-generator.appspot.com",
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
