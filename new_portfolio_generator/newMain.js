92dbb284450f6d7ff1e41e8e0041b56683331359
gallowag <gallowaygreer@gmail.com>
2017-04-04 12:16:46 -0600

    working on update courses function

--- new_portfolio_generator/newMain.js
+++ new_portfolio_generator/newMain.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 
 //global students
 var students = 0;
+var teachers =[];
 var courseRank = 0;
 var courses = 0;
 var oldCourses = 0;
@@ -20,15 +21,21 @@ var Student = function Student(name, full_time_weight, ticket_count){
     this.teachers = [];
 }
 
-var Course = function Course(name, course_lead, sections, department /*optional*/ ) {
+var Course = function Course(name, teacher, sections, department /*optional*/ ) {
     this.name = name;
-    this.courseLead = course_lead;
-    this.sections = sections;
-    this.ticket = 0;
-    this.weight = 0;
     if (department) this.department = department;
+    this.teacher = teacher;
+    this.sections = sections;
+    this.oldStudent = 0;
+    this.ticket = 0;
+    this.weight = 0;   
 }
 
+var Teacher = function(name) {
+    this.name = name;
+    this.courses = [];
+    this.weight = 0;
+}
 
 /*********************************************************
 * name: read
@@ -37,10 +44,10 @@ var Course = function Course(name, course_lead, sections, department /*optional*
 *********************************************************/
 function read() {
     //read files
-    studentContents = fs.readFileSync('\Test_CSV/\/Student Rank (Sept 30).csv').toString();
-    courseRankContents = fs.readFileSync('\Test_CSV/\/Course Rank.csv').toString();
-    courseInfoContents = fs.readFileSync('\Test_CSV/\/Course Variant List.csv').toString();
-    lastSemester = fs.readFileSync('\Test_CSV/\/lastSemester.csv').toString();
+    var studentContents = fs.readFileSync('\Test_CSV/\/Student Rank (Sept 30).csv').toString();
+    var courseRankContents = fs.readFileSync('\Test_CSV/\/Course Rank.csv').toString();
+    var courseInfoContents = fs.readFileSync('\Test_CSV/\/Course Variant List.csv').toString();
+    var lastSemester = fs.readFileSync('\Test_CSV/\/lastSemester.csv').toString();
 
     //turn CSV strings into object arrays
     students = d3Dsv.csvParse(studentContents, function(data) {
@@ -53,6 +60,8 @@ function read() {
     
     courseRank = d3Dsv.csvParse(courseRankContents);
     oldCourses = d3Dsv.csvParse(lastSemester);
+    
+   
 }
 
 /*********************************************************
@@ -66,23 +75,49 @@ function read() {
 * output: updated students
 **********************************************************/
 function updateCourseInfo() {
-    var sortedCourses = [];
-    //go through old students courses
+    
+    //update course objects ticket number and old student
     for (var i = 0; i < courses.length; i++) {
-        //go through current student courses
-        for (var j = 0; j < oldCourses.length; j++) {
-            //if they're the same course find student
-            if (oldCourses[j].Course === courses[i].name) {
-                sortedCourses.push(oldCourses[j]);
+        for (var j = 0; j < courseRank.length; j++) {
+            if (courses[i].name === courseRank[j].Course) {
+                courses[i].ticket = courseRank[j].Ticket_Count;
+            }
+        }
+        for (var k = 0; k < oldCourses.length; k++) {
+            if (courses[i].name === oldCourses[k].Course) {
+                courses[i].oldStudent = oldCourses[k].Student;
             }
         }
     }
-    console.log("before", courses.length);
-    console.log("after", sortedCourses.length);
     
-    courses = sortedCourses;
-    
-   
+    //teachers
+    var tempTeacher = 0;
+    //console.log(courses);
+    //console.log(courses);
+    for (i = 0; i < courses.length; i++) {
+        tempTeacher = new Teacher(courses[i].teacher)
+        //console.log(tempTeacher);
+        for (j = i; j < courses.length; j++) {
+            if ((courses[i].teacher === courses[j].teacher) &&
+                (courses[i].oldStudent === courses[j].oldStudent)) {
+                //console.log(courses[j]);
+                tempTeacher.courses.push(courses[j]);
+                courses.splice(j, 1);
+            } 
+        }
+        teachers.push(tempTeacher);
+    }
+    console.log("teachers:", teachers);
+    //console.log(courses);
+    //give students teachers
+    /*for (i = 0; i < students.length; i++) {
+        for(j = 0; j < teachers.length; j++) {
+            if (students[i].name === teachers[j].oldStudent) {
+                students[i].teachers.push(teachers[j]);
+            }
+        }
+    }*/
+
     //console.log(students);
     //find matching courses on course list
     //add matching course list to temp course list
@@ -98,7 +133,7 @@ function updateCourseInfo() {
                         console.log("error");
                     }
                 }*/
-    console.log(courses);
+    //console.log(students);
     
 }
 
